638adaa22e650934cc84ce40d6caab377e69cf05
liupeng <liupeng@dp.tech>
2023-09-21 13:50:11 +0800

    fix chemical item

--- streamlit_chemical_flow/streamlit_chemical_item/frontend/src/MoleculeItem.tsx
+++ streamlit_chemical_flow/streamlit_chemical_item/frontend/src/MoleculeItem.tsx
@@ -1,9 +1,10 @@
 import {
-  ComponentProps,
-  Streamlit,
-  withStreamlitConnection,
+    ComponentProps,
+    Streamlit,
+    withStreamlitConnection,
 } from "streamlit-component-lib"
 
+import {FullScreen, useFullScreenHandle} from "react-full-screen"
 import Molecule from "./Molecule";
 import React from "react";
 import "./molecule-item.css";
@@ -16,15 +17,27 @@ const onClick = (data: MoleculeItemData) => {
     console.log("clicked", data);
     Streamlit.setComponentValue(data);
 }
+
 function MoleculeItem({args}: ComponentProps): React.JSX.Element {
     const height = args.height || '100px';
+    const handler = useFullScreenHandle()
     Streamlit.setFrameHeight(height)
+
     return (
-            // set onclick to div onclick event
-            <div onClick={() => onClick(args.value)} className="molecule-item">
-                <Molecule smiles={args.value} />
-            </div>
-    );
+        <div>
+           {/*<button className="fullscreen-button" onClick={handler.enter}>*/}
+           {/*     <span></span>*/}
+           {/*     <span></span>*/}
+           {/*     <span></span>*/}
+           {/*     <span></span>*/}
+           {/* </button>*/}
+            <FullScreen className="myfullscreen" handle={handler}>
+                <div onClick={handler.enter} className="molecule-item">
+                    <Molecule smiles={args.value}/>
+                </div>
+            </FullScreen>
+        </div>
+    )
 };
 
 export default withStreamlitConnection(MoleculeItem);
